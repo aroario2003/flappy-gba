e592f696e73ada9aad8a7f2fdd87baa01dbcc7f3
Marie <asatterl@mail.umw.edu>
2024-04-17 17:40:47 -0400

    Removed extra while, other stuff too

--- src/main.c
+++ src/main.c
@@ -307,18 +307,14 @@ int main() {
     theSprite = sprite_init(100, 100, SIZE_16_16, 0, 0, 12, 0);
 
     handle_start();
-
-    Game game;
-    game.game_started = 0;
-    game.game_in_progress = 1;
-    game.game_ended = 0;
-
 #define SPRITE_ENABLE 0x1000
 #define SPRITE_MAP_1D 0x40
-    while (1) {
         /* we set the mode to mode 0 with bg0 on */
         *display_control = MODE0 | BG0_ENABLE | BG1_ENABLE | SPRITE_ENABLE | SPRITE_MAP_1D;
         /* store ints if game has started or is in progress or has ended. 0 = false, 1 = true */
+        int game_started = 0;
+        int game_in_progress = 1;
+        int game_ended = 0;
         /* setup the background 0 */
         setup_background(0);
         //Will save the value of the last background. Used to change if the background changes in-game.
@@ -333,32 +329,35 @@ int main() {
         bird_init(bird);
         //Counter. Counts the amount of ticks we have gone along.
         int counter = 0;
+        Sprite* sprite2;
+        sprite2 = sprite_init(100, 100, SIZE_16_16, 0, 0, 34, 0);
         /* loop forever */
         while (1) {
             //Kill switch 
-            if (button_pressed(BUTTON_SELECT) && game.game_ended == 0) {
+            if (button_pressed(BUTTON_SELECT) && game_ended == 0) {
                 *display_control = MODE0 | BG0_ENABLE | BG1_ENABLE | BG2_ENABLE;
                 setup_endscreen();
-                game.game_ended = 1;
-                game.game_in_progress = 0;
+                game_ended = 1;
+                game_in_progress = 0;
             }
             //Start again if you die
-            if (button_pressed(BUTTON_START) && game.game_ended == 1) {
-                game.game_started = 1;
-                game.game_ended = 0;
+            if (button_pressed(BUTTON_START) && game_ended == 1) {
+                game_started = 1;
+                game_ended = 0;
                 break;
             }
-          //   pipe_move(pipe);
-          //   pipe_move(pipe2);
+
+           
             //TODO: Make bird move properly
         if (button_pressed(BUTTON_A)){
-            if (bird->y <= HEIGHT/2){
-                 *display_control = 0;
+            if (bird->y-1 >= 16){
             }
-            sprite_move(theSprite, 0, -1);
-            bird->y = bird->y - 1; 
+            else{
+                sprite_move(theSprite, 0, -1);
+                bird->y = bird->y - 1; 
+            }
         }
-
+        sprite_move(sprite2, -1, 0);
             //Calls a function to update the bird's position on-screen 
             bird_update(bird);
             //Increment counter.
@@ -389,4 +388,3 @@ int main() {
             delay(50);
         }
     }
-}
